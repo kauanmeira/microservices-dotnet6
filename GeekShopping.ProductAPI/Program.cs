234c8cac2b68d28606a366f56ec227c2572b79e8
kauan.meira <kauangabriel.meira123@gmail.com>
2024-09-14 10:09:21 -0300

    DataBase Config

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,11 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
 
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 // Add services to the container.
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(5,0,29)))
+    );
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+});
+        
 
 var app = builder.Build();
 
