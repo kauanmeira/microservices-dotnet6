e263cac8427c4bf3f8506a6c438047fa2656ecdf
kauan.meira <kauangabriel.meira123@gmail.com>
2024-10-15 13:39:18 -0300

    Implementação Seed de Produtos

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace GeekShopping.ProductAPI.Controllers
         public async Task<ActionResult<ProductDTO>> FindById(long id)
         {
             var product = await _repository.FindById(id);
-            if (product == null) return NotFound();
+            if (product.Id <= 0) return NotFound();
             return Ok(product);
         }
 
